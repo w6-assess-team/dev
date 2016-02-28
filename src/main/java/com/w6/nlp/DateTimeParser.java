ed62877b3d735590e7439e61346d16798bb81864
Arseny <arsenytsypushkin@gmail.com>
2016-02-28 18:51:16 +0300

    Date class + tests
    Added tests and integration with parser

--- src/main/java/com/w6/nlp/DateTimeParser.java
+++ src/main/java/com/w6/nlp/DateTimeParser.java
@@ -1,37 +1,28 @@
-/**
- * Created by arseny on 25.02.16.
- */
+package com.w6.nlp;
+
 import edu.stanford.nlp.simple.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Class to parse date.
- */
+import javax.validation.constraints.NotNull;
 
 public class DateTimeParser {
-    /**
-     * String date parser.
-     * @param text    This is the text to parse.
-     */
 
-    public static List<String> parseDateAndDateFromString(String text) throws Exception {
-        if(text != null) {
-            List<String> dates = new ArrayList<String>();
-            Document doc = new Document(text);
-            for (Sentence sent : doc.sentences()) {
-                List<String> words = sent.lemmas();
-                List<String> tags = sent.nerTags();
-                for (int i = 0; i < tags.size(); ++i) {
-                    if (tags.get(i).equals("DATE")) {
-                        dates.add(words.get(i));
-                    }
+    @NotNull
+    public static List<String> parseDateAndDateFromString(String text) {
+    
+        List<String> dates = new ArrayList<String>();
+        Document doc = new Document(text);
+        for (Sentence sent : doc.sentences()) {
+            List<String> words = sent.lemmas();
+            List<String> tags = sent.nerTags();
+            for (int i = 0; i < tags.size(); ++i) {
+                if (tags.get(i).equals("DATE")) {
+                    dates.add(words.get(i));
                 }
             }
-            return dates;
-        } else {
-            throw new Exception("text is empty!");
         }
+        return dates;
     }
 }
