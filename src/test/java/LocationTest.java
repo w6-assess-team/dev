31c4462f310289fa22ea09a518fd79a7dbf0bb63
Arseny <arsenytsypushkin@gmail.com>
2016-02-28 20:51:06 +0300

    Names of tests changed

--- src/test/java/LocationTest.java
+++ src/test/java/LocationTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 public class LocationTest {
 
     @Test
-    public void testLocation0() {
+    public void testCityAndCountry() {
 		List<String> res = LocationParser.parseLocationFromString("World powers have agreed to seek" +
 				" a nationwide \"cessation of hostilities\" in Syria to begin in a week's time, after talks in Munich, Germany.");
 		List<String> correct= new ArrayList<String>();
@@ -20,7 +20,7 @@ public class LocationTest {
     }
     
     @Test
-    public void testLocation1() {
+    public void testCountry() {
 		List<String> res = LocationParser.parseLocationFromString("It was in Russia.");
 		List<String> correct= new ArrayList<String>();
 		correct.add("Russia");
@@ -28,7 +28,7 @@ public class LocationTest {
     }
     
     @Test
-    public void testLocation2() {
+    public void testComplexCity() {
 		List<String> res = LocationParser.parseLocationFromString("Hellow to you from New-York!");
 		List<String> correct= new ArrayList<String>();
 		correct.add("New-York");
