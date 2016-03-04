953120aefab43f6e79aa33c4d746f900bc83fc09
howareyouman <arsenytsypushkin@gmail.com>
2016-03-04 18:08:26 +0300

    modified:   ViolentVerbsParser.java

--- src/main/java/com/w6/nlp/ViolentVerbsParser.java
+++ src/main/java/com/w6/nlp/ViolentVerbsParser.java
@@ -1,5 +1,11 @@
 package com.w6.nlp;
 
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.process.Tokenizer;
+import edu.stanford.nlp.process.TokenizerFactory;
 import static edu.stanford.nlp.util.logging.RedwoodConfiguration.Handlers.file;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -13,6 +19,7 @@ public class ViolentVerbsParser {
     
    static Set violentWords = null;
    static String globalPpath = "/src/resources/violentVerbsDictionary.txt";
+   static final String PCG_MODEL = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
    
    private static void setViolentDictionary() throws IOException{
        if(violentWords == null){
@@ -29,14 +36,26 @@ public class ViolentVerbsParser {
         }
    }
    
+   private  static List<CoreLabel> tokenize(String str) {
+        Tokenizer<CoreLabel> tokenizer =
+                tokenizerFactory.getTokenizer(
+                        new StringReader(str));
+        return tokenizer.tokenize();
+    }
+   
    public static List<String> getAllViolentVerbs(String text){
-       try {
+        try {
           setViolentDictionary();
-       } catch (Exception e) {
-       }
-       List<String> result = new ArrayList<>();
-       
-       return result;
+        } catch (Exception e) {}
+        
+        List<String> result = new ArrayList<>();
+    
+
+        TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(), "invertible=true");
+        LexicalizedParser parser = LexicalizedParser.loadModel(PCG_MODEL);
+        List<CoreLabel> tokens = tokenize(str);
+    
+        return result;
        
    }
            
