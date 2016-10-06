d4039cfd5ca58c89127ee3a3998d19285bf710ba
Ilia <C:\Users\Ilya\AppData\Roaming\The Bat!>
2016-10-07 01:47:07 +0300

    Add beans for parsers

--- src/main/java/com/w6/config/AppContext.java
+++ src/main/java/com/w6/config/AppContext.java
@@ -1,5 +1,11 @@
 package com.w6.config;
 
+import com.w6.nlp.*;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.process.TokenizerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +16,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import java.io.IOException;
+
 @Configuration
-@ComponentScan(basePackages = {"com.w6.controller"})
+@ComponentScan(basePackages = {"com.w6"})
 @EnableWebMvc
 @EnableScheduling
 @ImportResource("classpath:applicationContext.xml")
@@ -29,5 +37,42 @@ public class AppContext {
         viewResolver.setSuffix(VIEW_RESOLVER_SUFFIX);
 
         return viewResolver;
+    }
+
+
+    @Bean
+    public MySolrClient mySolrClient() {
+        return new MySolrClient();
+    }
+
+
+    @Bean
+    public LexicalizedParser lp() {
+        return LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
+    }
+
+    @Bean
+    public ViolentVerbsParser violentVerbsParser() throws IOException {
+        return new ViolentVerbsParser(lp());
+    }
+
+    @Bean
+    public WeaponsParser weaponsParser() throws IOException {
+        return new WeaponsParser(lp());
+    }
+
+    @Bean
+    public CountryParser countryParser() throws IOException {
+        return new CountryParser();
+    }
+
+    @Bean
+    public TokenizerFactory<CoreLabel> tokenizerFactory() {
+        return PTBTokenizer.factory(new CoreLabelTokenFactory(), "");
+    }
+
+    @Bean
+    public Parser parser() throws IOException {
+        return new Parser();
     }
 }
