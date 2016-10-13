d8095d23fb0c9540e20db66e39242e12831b83d9
Ilya Orlov <iorlov@GDSpb0719.local>
2016-10-14 01:32:52 +0300

    Remove resolver view from AppConext

--- src/main/java/com/w6/config/AppContext.java
+++ src/main/java/com/w6/config/AppContext.java
@@ -7,38 +7,14 @@ import edu.stanford.nlp.process.CoreLabelTokenFactory;
 import edu.stanford.nlp.process.PTBTokenizer;
 import edu.stanford.nlp.process.TokenizerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 import java.io.IOException;
 
 @Configuration
-@ComponentScan(basePackages = {"com.w6"})
-@EnableWebMvc
 @EnableScheduling
-@ImportResource("classpath:applicationContext.xml")
 public class AppContext {
-
-    private static final String VIEW_RESOLVER_PREFIX = "/WEB-INF/jsp/";
-    private static final String VIEW_RESOLVER_SUFFIX = ".jsp";
-
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix(VIEW_RESOLVER_PREFIX);
-        viewResolver.setSuffix(VIEW_RESOLVER_SUFFIX);
-
-        return viewResolver;
-    }
-
 
     @Bean
     public MySolrClient mySolrClient() {
