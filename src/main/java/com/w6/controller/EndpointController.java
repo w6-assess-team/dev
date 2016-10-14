f9a5dad9b4f170233747dae68157133ce0c0c657
Ilya Orlov <iorlov@GDSpb0719.local>
2016-10-14 03:07:40 +0300

    Add login and logout end points

--- src/main/java/com/w6/controller/EndpointController.java
+++ src/main/java/com/w6/controller/EndpointController.java
@@ -12,11 +12,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class EndpointController {
@@ -124,11 +130,6 @@ public class EndpointController {
         
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String homePage() {
-        return "redirect:/input";
-    }
-
     @RequestMapping(value = "/input", method = RequestMethod.GET)
     public String displayInput()
     {
@@ -188,6 +189,19 @@ public class EndpointController {
         modelAndView.addObject("month", month);
         
         return modelAndView;
-    }  
-    
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logout (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
+    }
 }
