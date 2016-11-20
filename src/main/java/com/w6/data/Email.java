b8e951502585fbb0fc9345390b12506f9c3397df
Ilya Orlov <iorlov@GDSpb0719.local>
2016-11-20 03:26:27 +0300

    Add solr annotations and getters/setters to entity classes

--- src/main/java/com/w6/data/Email.java
+++ src/main/java/com/w6/data/Email.java
@@ -1,17 +1,36 @@
 package com.w6.data;
 
+import org.apache.solr.client.solrj.beans.Field;
+import org.springframework.data.solr.core.mapping.SolrDocument;
+
+@SolrDocument(solrCoreName = "email")
 public class Email {
+
+    @Field
     private long id;
+
+    @Field
     private String subject;
+
+    @Field
     private String  date;
+
+    @Field
     private String from;
+
+    @Field
     private String text;
+
+    @Field
     private Boolean used;
     //todo: add boolean "used"
-    
-    public Email(long id, String date, String subject, 
-             String text, String from, Boolean used) 
-    {
+
+
+    public Email() {
+    }
+
+    public Email(long id, String date, String subject,
+                 String text, String from, Boolean used) {
         this.id = id;
         this.date = date;
         this.subject = subject;
@@ -19,5 +38,52 @@ public class Email {
         this.from = from;
         this.used = used;
     }
-    
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Boolean getUsed() {
+        return used;
+    }
+
+    public void setUsed(Boolean used) {
+        this.used = used;
+    }
 }
